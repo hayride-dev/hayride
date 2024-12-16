314e57b9ac5df32717972a27048617c1a94703dd
jeff1010322 <jeff1010322@gmail.com>
2024-12-16 13:52:17 -0500

    feat: implement initial websocket support for chat view

--- crates/hayride-ui/src/views/chat.rs
+++ crates/hayride-ui/src/views/chat.rs
@@ -1,7 +1,59 @@
 use leptos::prelude::*;
+use leptos::web_sys::console;
+use leptos_use::{
+  core::ConnectionReadyState, use_websocket, UseWebSocketReturn,
+};
+use codee::string::FromToStringCodec;
 
 #[component]
 pub fn Chat() -> impl IntoView {
+    // ----------------------------
+    // use_websocket
+    // ----------------------------
+
+    let UseWebSocketReturn {
+        ready_state,
+        message,
+        send,
+        open,
+        close,
+        ..
+    } = use_websocket::<String, String, FromToStringCodec>("ws://localhost:8081");
+
+    let (input, set_input) = signal(String::new());
+    let (messages, set_messages) = signal(Vec::new());
+
+    // Update the messages signal when a new message is received
+    Effect::new(move |_| {
+        if let Some(msg) = message.get() {
+            console::log_1(&"Received message".into());
+            set_messages.update(|msgs| msgs.push(format!("{}", msg)));
+        }
+    });
+
+    Effect::new(move |_| {
+        console::log_1(&format!("Current message: {:?}", message.get()).into());
+    });
+
+    Effect::new(move |_| {
+        console::log_1(&format!("WebSocket Ready State: {:?}", ready_state.get()).into());
+    });
+
+    let send_message = move |_| {
+      let msg = input.get();
+        if !msg.is_empty() {
+            console::log_1(&"Sending message".into());
+
+            send(&msg.clone());
+            set_input.set(String::new());
+            // set_history.update(|history: &mut Vec<_>| history.push(format!("[send]: {:?}", m)));
+        }
+    };
+
+    let status = move || ready_state.get().to_string();
+
+    let connected = move || ready_state.get() == ConnectionReadyState::Open;
+
     view! {
         <main id="content">
             <div class="relative h-screen">
@@ -16,9 +68,20 @@ pub fn Chat() -> impl IntoView {
                     </div>
                 </div>
 
+                <h2>"Messages"</h2>
+                <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                  {move || messages.get().iter().map(|msg| view! { <li>{msg.clone()}</li> }).collect::<Vec<_>>()}
+                </p>
+
                 // Input -->
                 <div class="relative">
-                  <textarea class="p-4 pb-12 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Ask me anything..."></textarea>
+                  <input 
+                    value=input
+                    on:input=move |ev| set_input.set(event_target_value(&ev))
+                    class="p-4 pb-12 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                    placeholder="Ask me anything..."
+                  >
+                  </input>
 
                   // Toolbar -->
                   <div class="absolute bottom-px inset-x-px p-2 rounded-b-lg bg-white dark:bg-neutral-900">
@@ -57,7 +120,7 @@ pub fn Chat() -> impl IntoView {
                         // End Mic Button -->
 
                         // Send Button -->
-                        <button type="button" class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-white bg-blue-600 hover:bg-blue-500 focus:z-10 focus:outline-none focus:bg-blue-500">
+                        <button type="button" on:click=send_message class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-white bg-blue-600 hover:bg-blue-500 focus:z-10 focus:outline-none focus:bg-blue-500">
                           <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
                           </svg>
