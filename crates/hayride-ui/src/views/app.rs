f6943c80b98f7c0e74850a32037f6e103019dda7
elewis787 <ethanlewis787@gmail.com>
2024-12-17 11:49:56 -0800

    feat: add avatar, update sidebar and add fonts

--- crates/hayride-ui/src/views/app.rs
+++ crates/hayride-ui/src/views/app.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 
 use super::chat::Chat;
 use crate::components::sidebar::Sidebar;
-
+use crate::components::avatar::Avatar;
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,10 +17,13 @@ extern "C" {
 pub fn App() -> impl IntoView {
     view! {
         <div class="flex h-screen w-screen">
+        <div class="absolute top-0 right-0 m-4 w-12 z-10">
+            <Avatar img_src="https://avatars.githubusercontent.com/u/10167943?v=4".to_string()/>
+        </div>
         <aside class="fixed top-0 left-0 h-full w-64 bg-base-200">
             <Sidebar/>
         </aside>
-        <main id="content" class="flex-1 ml-64 overflow-auto">
+        <main id="content"  class="flex flex-col flex-1 ml-64 overflow-auto">
             <Router>
                 <Routes fallback=|| view! { <div>"Page not found"</div> }>
                     <Route path=path!("/") view=Chat/>
