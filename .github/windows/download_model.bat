042a341ce3de3721dda93702f2fef1f0e30b16df
jeff1010322 <jeff1010322@gmail.com>
2025-06-11 16:32:27 -0400

    feat: add download_model.bat and custom action to download model during windows install

---
+++ .github/windows/download_model.bat
@@ -0,0 +1,10 @@
+@echo off
+set "TARGET=%APPDATA%\hayride\models"
+mkdir "%TARGET%" 2>nul
+
+if exist "%TARGET%\Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf" (
+  echo File already exists, skipping download.
+  exit /b 0
+)
+
+curl -L "https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf" -o "%TARGET%\Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf"
